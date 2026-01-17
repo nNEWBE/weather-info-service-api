692b975054dceeca740bc1d4fcae483bc652f796
Shuvo Chandra Deb Nath <shuvochandra999@gmail.com>
2026-01-17 21:03:38 +0600

    Project initialized

---
+++ src/main/java/com/example/weather_info_service/WeatherInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.weather_info_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weather_info_service/WeatherInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weather_info_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
